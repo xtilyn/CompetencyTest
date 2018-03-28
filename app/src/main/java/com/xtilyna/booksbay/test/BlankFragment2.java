f216edf955b7bde315255553014fe2d2d8b04bd7
Christilyn A <christilynarjona@outlook.com>
2018-03-28 13:05:56 -0600

    scroll fix

--- app/src/main/java/com/xtilyna/booksbay/test/BlankFragment2.java
+++ app/src/main/java/com/xtilyna/booksbay/test/BlankFragment2.java
@@ -4,6 +4,7 @@ package com.xtilyna.booksbay.test;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.NestedScrollView;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
